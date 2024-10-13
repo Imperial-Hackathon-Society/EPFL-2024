da047f7ede741fbfce0a2ad14e55f1ab94e5d6e5
TomSmail <thomas.c.smail@gmail.com>
2024-10-13 02:22:01 +0200

    feat: full flow from background

--- dapp/encryption/encrypt.py
+++ dapp/encryption/encrypt.py
@@ -1,53 +1,62 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
+from concrete.ml.deployment import FHEModelClient
+import numpy as np
+import requests
+import json
 import base64
-import hashlib
-from Crypto import Random
-from Crypto.Cipher import AES
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+FHE_DIRECTORY = './dapp/encryption/tmp/fhe_client_server_files/'
+URL = "http://127.0.0.1:5000/predict"
 
-class AESCipher(object):
+labels = [
+    "Hypertension",
+    "Diabetes",
+    "Asthma",
+    "Coronary Artery Disease",
+    "Chronic Kidney Disease",
+    "Obesity",
+    "Anemia",
+    "Osteoporosis",
+    "Hyperlipidemia",
+    "None"
+]
 
-    def __init__(self, key):
-        self.bs = AES.block_size
-        self.key = hashlib.sha256(key.encode()).digest()
+client = FHEModelClient(path_dir=FHE_DIRECTORY, key_dir="/tmp/keys_client")
+serialized_evaluation_keys = client.get_serialized_evaluation_keys()
 
-    def encrypt(self, raw):
-        raw = self._pad(raw)
-        iv = Random.new().read(AES.block_size)
-        cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return base64.b64encode(iv + cipher.encrypt(raw.encode()))
 
-    def decrypt(self, enc):
-        enc = base64.b64decode(enc)
-        iv = enc[:AES.block_size]
-        cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return AESCipher._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
+def helper(data):
+    encrypted_data = client.quantize_encrypt_serialize(data)
+    # Make new request to the server
+    try:
+        response = requests.post(URL, headers={"Content-Type": "application/json"}, data=json.dumps({"value": base64.b64encode(encrypted_data).decode('ascii')}))
+        encrypted_result = response.json()["encrypted_result"]
+        encrypted_result = base64.b64decode(encrypted_result)
+    except Exception as e: 
+        print(e)
+        return json.dumps({"value": "None"}) 
 
-    def _pad(self, s):
-        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
+    decrypted_data = client.deserialize_decrypt_dequantize(encrypted_result)
 
-    @staticmethod
-    def _unpad(s):
-        return s[:-ord(s[len(s)-1:])]
-    
-@app.route("/encrypt", methods=["POST"])
+    result = labels[np.argmax(decrypted_data)]
+    return json.dumps({"value": result}) 
+
+# encrypt the data
+@app.route("/", methods=["POST"])
 @cross_origin()
-# Encrypt data using RSA then return to frontend 
 def encrypt():
-    key = request.json["key"]
     data = request.json["value"]
-    return jsonify(AESCipher(key).encrypt(data).decode("utf-8")) 
+    return helper(data)
 
-@app.route("/decrypt", methods=["POST"])
-@cross_origin()
-# Decrypt data using RSA then return to frontend
-def decrypt():
-    key = request.json["key"]
-    data = request.json["value"]
-    return jsonify(AESCipher(key).decrypt(data).decode("utf-8"))
 
-app.run(port=7385)
+
+
+
+if __name__ == "__main__":
+    app.run(port=5001)
+    # data = np.random.rand(1, 20)
+    # print(helper(data))
--- fhe/fheProd.py
+++ fhe/fheProd.py
@@ -1,6 +1,14 @@
 import pandas as pd
 import numpy as np
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 from concrete.ml.deployment import FHEModelClient, FHEModelServer
+import base64
+
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 labels = [
     "Hypertension",
@@ -15,23 +23,37 @@ labels = [
     "None"
 ]
 
-
 FHE_DIRECTORY = 'tmp/fhe_client_server_files/'
+
 # Setup the client
 client = FHEModelClient(path_dir=FHE_DIRECTORY, key_dir="/tmp/keys_client")
 serialized_evaluation_keys = client.get_serialized_evaluation_keys()
-
-# Client pre-processes new data
-X_new = np.random.rand(1, 2)
-encrypted_data = client.quantize_encrypt_serialize(X_new)
 
 # Setup the server
 server = FHEModelServer(path_dir=FHE_DIRECTORY)
 server.load()
 
-# Server processes the encrypted data
-encrypted_result = server.run(encrypted_data, serialized_evaluation_keys)
+@app.route('/predict', methods=['POST'])
+@cross_origin()
+def predict():
+    try:
+        # Get the encrypted data from the request
+        encrypted_data = request.json['value']
+        # Convert from string to bytes
+        # print(encrypted_data)
+        encrypted_data = base64.b64decode(encrypted_data)
+        # print(encrypted_data)
+        # Server processes the encrypted data
+        encrypted_result = server.run(encrypted_data, serialized_evaluation_keys)
+        print(encrypted_result)
+        # Return the prediction as a JSON response
+        results = encrypted_result
 
-# Client decrypts the result
-result = client.deserialize_decrypt_dequantize(encrypted_result)
-print(f"Prediction: {result}")
+        print(results)
+        return jsonify({'encrypted_result': base64.b64encode(results).decode('ascii')}), 200
+    except Exception as e:
+        print(e)
+        return jsonify({'error': str(e)}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
